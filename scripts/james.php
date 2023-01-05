9110fc1d6cdcabe813bc039edc4a4faea922e834
Frédéric Glorieux <frederic.glorieux@fictif.org>
2023-01-05 17:31:09 +0100

    Balisage automatisée des articles, à relire

--- scripts/james.php
+++ scripts/james.php
@@ -11,9 +11,9 @@ Log::setLogger(new LoggerCli(LogLevel::DEBUG));
 
 
 $preg = Parse::pcre_tsv(__DIR__ . '/jamesfr_norm.tsv');
-$dst_dir = dirname(__DIR__) . '/work/james/';
+$dst_dir = dirname(__DIR__) . '/work/';
 Filesys::mkdir($dst_dir);
-$glob = dirname(__DIR__) . '/xml/medict00216x*.xml';
+$glob = dirname(__DIR__) . '/xml/medict00216x??.xml';
 foreach (glob($glob) as $src_tei) {
     $dst_tei = $dst_dir . basename($src_tei);
     $xml = file_get_contents($src_tei);
